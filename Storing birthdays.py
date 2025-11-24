70bc11a9446ad296675ed7b0e784168802b3894c
reduzoth65-create <reduzoth65@gmail.com>
2025-11-24 22:24:31 +0530

    Add files via upload

---
+++ Storing birthdays.py
@@ -0,0 +1,10 @@
+age1=5
+age2=13
+age3=43
+age4=41
+age5=17
+print("My brother age is",age1)
+print("My age is ",age2)
+print("My father age is",age3)
+print("My mother age is",age4)
+print("My sister age is",age5)
