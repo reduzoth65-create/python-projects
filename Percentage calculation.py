940af4502b81745771552495fb7f6af40598c648
reduzoth65-create <reduzoth65@gmail.com>
2025-12-01 21:38:04 +0530

    Add files via upload

---
+++ Percentage calculation.py
@@ -0,0 +1,10 @@
+print("Enter marks obtained in 4 subjects: ")
+math=int(input("maths:"))
+english=int(input("english:"))
+science=int(input("science:"))
+hindi=int(input("hindi:"))
+sum=math+english+science+hindi 
+print("sum of math,english,science,hindi")
+perc=(sum/400)*100
+print(end="percentage marks= ")
+print(perc)
