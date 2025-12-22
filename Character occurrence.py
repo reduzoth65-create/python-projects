70628f7fa0a2d6fa2d471f51702b4958180e7d42
reduzoth65-create <reduzoth65@gmail.com>
2025-12-22 20:43:06 +0530

    Add files via upload

---
+++ Character occurrence.py
@@ -0,0 +1,11 @@
+string= input("Please enter your own word:")
+char=input("Please enter your own character:")
+i=0
+count=0
+while(i<len(string)):
+    if(string[i]==char):
+        count=count+1
+        i=i+1
+
+
+print("the total number of times",char,"has occurred=",count)
