bd92ab447e289cff9e29559a84ea47a8927c1dbf
SJ CODER <reduzoth65@gmail.com>
2026-05-20 19:34:54 +0530

    Add files via upload

---
+++ Housing Rent Prediction.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns 
+
+df=pd.read_csv("USA_Housing.csv")
+print(df.head(10))
+
+print(df.info())
+print(df.describe())
+print(df.columns)
+
+sns.pairplot(df.select_dtypes(include=[np.number]))
+
+sns.heatmap(df.select_dtypes(include=[np.number]).corr(),annot=True)
+
+plt.show()
